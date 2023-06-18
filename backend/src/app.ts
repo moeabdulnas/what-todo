657b12102833f1326c538cc351890ffb4c6d2ff8
Mohamed Abdulnassir <mohab918@student.liu.se>
2023-06-18 18:58:57 +0200

    Added utils for verifying env variables. Added dotenv

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,24 @@
 import 'dotenv/config';
 import express from 'express';
+import mongoose from 'mongoose';
+import env from './utils/validateEnv';
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = env.PORT;
+
+const MONGO = env.MONGO_STRING;
+
+mongoose.connect(MONGO)
+    .then(() => {
+        console.log('Connected to mongodb')
+        app.listen(PORT, () => {
+            console.log('Server listening on port: ' + PORT);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 app.get('/', (req, res) => {
     res.send('Hello world')!
-})
-
-app.listen(PORT, () => {
-    console.log('Server listening on port: ' + PORT);
 })
