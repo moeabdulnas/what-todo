f49b419ec8da173653b83b877eb3109dcdf8fe44
Mohamed Abdulnassir <mohab918@student.liu.se>
2023-06-20 11:35:21 +0200

    Will do without tailwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import NavBar from './components/NavBar.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <NavBar/>
     <App />
   </React.StrictMode>,
 )
