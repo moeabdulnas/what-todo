89945e14d1d7c10ad301fd0526c0d07abc95e6f8
Mohamed Abdulnassir <mohab918@student.liu.se>
2023-06-20 11:07:26 +0200

    Tailwind setup.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { TodoModel } from './models/todo';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Todo as TodoModel } from './models/todo';
 import Todo from './components/Todo';
 
 function App() {
@@ -21,15 +20,7 @@ function App() {
   }, [todos]);
   return (
     <>
-      <Container>
-        <Row xs={1} md={2} xl={3} className='g-4'>
-          {todos.map( (todo) => (
-            <Col key={todo._id}>
-              <Todo todo={todo}/>
-            </Col>
-          ))}
-        </Row>
-      </Container>
+      
     </>
   )
 }
--- frontend/src/components/Todo.tsx
+++ frontend/src/components/Todo.tsx
@@ -1,5 +1,4 @@
-import { Card } from "react-bootstrap"
-import { Todo as TodoModel } from "../models/todo"
+import { Todo as TodoModel } from "../models/todo";
 
 // Type for the props of a Todo component
 interface TodoProps {
@@ -7,33 +6,17 @@ interface TodoProps {
 }
 
 const Todo = ({todo}: TodoProps) => {
-    const {
-        text,
-        done,
-        createdAt,
-        updatedAt
-    } = todo;
+    // const {
+    //     text,
+    //     done,
+    //     createdAt,
+    //     updatedAt
+    // } = todo;
 
     return (
         <>
-            <Card>
-                <Card.Body>
-                    <Card.Title>
-                        {text}
-                    </Card.Title>
-                    <Card.Text>
-                        {done ? <>Completed</>: <>Not done yet</>}
-                    </Card.Text>
-                </Card.Body>
-
-                <Card.Footer>
-                    {createdAt}
-                    {updatedAt}
-                </Card.Footer>
-
-            </Card>
         </>
     )
-}
+};
 
 export default Todo;
