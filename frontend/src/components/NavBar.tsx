7f565b810321af8e7ce850d1bb8bc527a58b898f
Mohamed Abdulnassir <mohab918@student.liu.se>
2023-06-21 12:01:31 +0200

    Started with the frontend

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,7 +1,12 @@
+import "./NavBar.css";
 
 const NavBar = () => {
     return (
-        <>        
+        <>
+            <div className="NavBar flex">
+                <h3 className="Logo">what-todo</h3>
+                <button className="LoginButton">Login</button>
+            </div>        
         </>
     )
 }
