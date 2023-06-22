ee63de36b13eb4022307785663924f6b35da1b20
Mohamed Abdulnassir <mohab918@student.liu.se>
2023-06-22 06:34:57 +0200

    NavBar done for now.

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -4,9 +4,12 @@ const NavBar = () => {
     return (
         <>
             <div className="NavBar flex">
-                <h3 className="Logo">what-todo</h3>
-                <button className="LoginButton">Login</button>
-            </div>        
+                <div className="LeftNavBar">
+                    <h3 className="Logo">what-todo?</h3>
+                    <button className="Button add">New todo</button>    
+                </div>
+                <button className="Button login">Login</button>
+            </div>
         </>
     )
 }
